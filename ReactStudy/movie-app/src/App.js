1fc277ab4296a2f59d4ce084863938466bbbb2c8
seulgi90 <seulgi6380@gmail.com>
2023-04-27 10:00:24 +0900

    coin tracker

--- ReactStudy/movie-app/src/App.js
+++ ReactStudy/movie-app/src/App.js
@@ -8,18 +8,18 @@ function App() {
     fetch("https://api.coinpaprika.com/v1/tickers")
       .then((response) => response.json())
       .then((json) => {
-        setCoins("json->",json);
+        setCoins(json);
         setLoading(false);
       });
   }, []);
-  return (
+  return ( 
     <div>
       <h1>The Coins! ({coins.length})</h1>
       {loading ? <strong>Loading...</strong> : null}
       <ul>
         {coins.map((coin) => (
             <li>
-              {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price}
+              {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
             </li>
           ))}
       </ul>
