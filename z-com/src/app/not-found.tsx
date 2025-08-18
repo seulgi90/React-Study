4fc4a218f13bc92e07727236e2ae12bd5507cbf2
seulgi90 <seulgi6380@gmail.com>
2025-08-18 22:38:48 +0900

    next.js 초기 설정

---
+++ z-com/src/app/not-found.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {NextPage} from "next";
+import Link from "next/link";
+
+const NotFound: NextPage = () => {
+    return (
+        <div>
+            <div>이 페이지는 존재하지 않습니다. 다른 페이지를 검색 하세요.</div>
+            <Link href="/search">검색</Link>
+        </div>
+    );
+};
+
+export default NotFound;
